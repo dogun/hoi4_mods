ada50db5506a665b809dbec59b702c82c9d06266
dogun <yuexuqiang@gmail.com>
2024-01-24 23:02:14 +0800

    u

--- tools/dds2png.php
+++ tools/dds2png.php
@@ -1,12 +1,27 @@
 <?php
-$path = 'D:/SteamLibrary/steamapps/common/Hearts of Iron IV/gfx/interface/goals';
+$path = 'D:/SteamLibrary/steamapps/common/Hearts of Iron IV/gfx/leaders';
+
+function ddd($path) {
 $d = dir($path);
-while (($f = $d->read()) !== false) {
-	if (!strstr($f, 'dds')) continue;
-	$file = $path.'/'.$f;
-	$new_file = 'c:/tmp/'.str_replace('.dds', '.png', $f);
-	$img = new Imagick();
-	$img->readImage($file);
-	$img->setFormat('PNG');
-	$img->writeImage($new_file);
+	while (($f = $d->read()) !== false) {
+		if ($f == '.' || $f == '..') continue;
+		$file = $path.'/'.$f;
+		if (is_dir($file)) {
+			ddd($file);
+			continue;
+		}
+		if (!strstr($f, 'dds')) continue;
+		echo $file."\n";
+		$new_file = 'c:/tmp/'.str_replace('.dds', '.png', $f);
+		try {
+			$img = new Imagick();
+			$img->readImage($file);
+			$img->setFormat('PNG');
+			$img->writeImage($new_file);
+		} catch (Exception $e) {
+			echo "   ".$e->getMessage()."\n";
+		}
+	}
 }
+
+ddd($path);
